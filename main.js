cda09602de7e17e283d9e9f55a2bcb03f0612cec
Maria-studyaccount <149117598+Maria-studyacc@users.noreply.github.com>
2023-12-18 10:44:12 +0200

    Practical assignment 2

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let gl;                         // The webgl context.
 let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
-let line;
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
@@ -15,18 +14,20 @@ function deg2rad(angle) {
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
-    this.iNormalBuffer = gl.createBuffer();
+    this.iVertexNormalBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function (vertices, normals) {
+    this.BufferData = function (vertices) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STREAM_DRAW);
-
         this.count = vertices.length / 3;
+    }
+    this.BufferNormalData = function (normals) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STREAM_DRAW);
     }
 
     this.Draw = function () {
@@ -34,10 +35,9 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
-        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribNormal);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormalVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormalVertex);
 
         gl.drawArrays(gl.TRIANGLES, 0, this.count);
     }
@@ -52,6 +52,7 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
+    this.iAttribNormalVertex = -1;
     // Location of the uniform specifying a color for the primitive.
     this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
@@ -72,15 +73,13 @@ function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     /* Set the values of the projection transformation */
-    // let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
-    const projVal = 17;
-    let projection = m4.orthographic(-projVal, projVal, -projVal, projVal, -projVal, projVal);
+    let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
 
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
 
     let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
-    let translateToPointZero = m4.translation(0, 0, -5);
+    let translateToPointZero = m4.translation(0, 0, -10);
 
     let matAccum0 = m4.multiply(rotateToPointZero, modelView);
     let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
@@ -88,12 +87,12 @@ function draw() {
     /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
     let modelViewProjection = m4.multiply(projection, matAccum1);
-    const normalMat = m4.identity();
-    m4.inverse(modelView, normalMat);
-    m4.transpose(normalMat, normalMat);
+    let normalMatrix = m4.identity();
+    m4.inverse(modelView, normalMatrix);
+    normalMatrix = m4.transpose(normalMatrix);
 
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
-    gl.uniformMatrix4fv(shProgram.iNormalMat, false, normalMat);
+    gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
 
     /* Draw the six faces of a cube, with different colors. */
     gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
@@ -101,90 +100,125 @@ function draw() {
     surface.Draw();
 }
 
-const { cos, sin, sqrt, pow, PI } = Math
+function generatePoint(u, v, a, b) {
+    const x = a * (b - Math.cos(u)) * Math.sin(u) * Math.cos(v);
+    const y = a * (b - Math.cos(u)) * Math.sin(u) * Math.sin(v);
+    const z = Math.cos(u);
+    vertexList.push(x, y, z);
+}
+function getPoint(u, v, a, b) {
+    const x = a * (b - Math.cos(u)) * Math.sin(u) * Math.cos(v);
+    const y = a * (b - Math.cos(u)) * Math.sin(u) * Math.sin(v);
+    const z = Math.cos(u);
+    return [x, y, z]
+}
+function generatePointsNormal(u, v, a, b) {
+    const steps = 30;
+    const uStep = (1 / steps) * Math.PI;
+    const vStep = (1 / steps) * Math.PI * 2;
+    let v0 = getPoint(u, v, a, b);
+    let v1 = getPoint(u + uStep, v, a, b);
+    let v2 = getPoint(u, v + vStep, a, b);
+    let v3 = getPoint(u - uStep, v + vStep, a, b);
+    let v4 = getPoint(u - uStep, v, a, b);
+    let v5 = getPoint(u - uStep, v - vStep, a, b);
+    let v6 = getPoint(u, v - vStep, a, b);
+    let v01 = m4.subtractVectors(v1, v0)
+    let v02 = m4.subtractVectors(v2, v0)
+    let v03 = m4.subtractVectors(v3, v0)
+    let v04 = m4.subtractVectors(v4, v0)
+    let v05 = m4.subtractVectors(v5, v0)
+    let v06 = m4.subtractVectors(v6, v0)
+    let n1 = m4.normalize(m4.cross(v01, v02))
+    let n2 = m4.normalize(m4.cross(v02, v03))
+    let n3 = m4.normalize(m4.cross(v03, v04))
+    let n4 = m4.normalize(m4.cross(v04, v05))
+    let n5 = m4.normalize(m4.cross(v05, v06))
+    let n6 = m4.normalize(m4.cross(v06, v01))
+    let n = [(n1[0] + n2[0] + n3[0] + n4[0] + n5[0] + n6[0]) / 6.0,
+    (n1[1] + n2[1] + n3[1] + n4[1] + n5[1] + n6[1]) / 6.0,
+    (n1[2] + n2[2] + n3[2] + n4[2] + n5[2] + n6[2]) / 6.0]
+    n = m4.normalize(n);
+    normalList.push(...n);
+}
+
+let vertexList = [];
+let normalList = [];
+
 function CreateSurfaceData() {
-    let vertexList = [];
-    const NUM_STEPS_U = 50,
-        NUM_STEPS_Z = 50,
-        MAX_U = PI * 2,
-        MAX_Z = 3,
-        STEP_U = MAX_U / NUM_STEPS_U,
-        STEP_Z = MAX_Z / NUM_STEPS_Z
-    for (let u = 0; u < MAX_U; u += STEP_U) {
-        for (let z = -3; z < MAX_Z; z += STEP_Z) {
-            let vertex = cassiniVertex(u, z)
-            vertexList.push(...vertex)
-            vertex = cassiniVertex(u + STEP_U, z)
-            vertexList.push(...vertex)
-            vertex = cassiniVertex(u, z + STEP_Z)
-            vertexList.push(...vertex)
-            vertexList.push(...vertex)
-            vertex = cassiniVertex(u + STEP_U, z)
-            vertexList.push(...vertex)
-            vertex = cassiniVertex(u + STEP_U, z + STEP_Z)
-            vertexList.push(...vertex)
+    // Значення параметра a
+    const a = 0.5;
+    // Значення параметра b
+    const b = 1;
+    // Кількість кроків циклу побудови
+    const steps = 30;
+
+    // Побудова "горизонтальних кіл" фігури
+    for (let i = 0; i <= steps; i++) {
+        const u = (i / steps) * Math.PI;
+        const uStep = (1 / steps) * Math.PI;
+        for (let j = 0; j <= steps; j++) {
+            const v = (j / steps) * Math.PI * 2;
+            const vStep = (1 / steps) * Math.PI * 2;
+            generatePoint(u, v, a, b);
+            generatePoint(u + uStep, v, a, b);
+            generatePoint(u, v + vStep, a, b);
+            generatePoint(u, v + vStep, a, b);
+            generatePoint(u + uStep, v, a, b);
+            generatePoint(u + uStep, v + vStep, a, b);
         }
     }
+
+
+    // Побудова "вертикальних кіл" фігури
+    // for (let i = 0; i <= steps; i++) {
+    //     const v = (i / steps) * Math.PI * 2;
+    //     for (let j = 0; j <= steps; j++) {
+    //         const u = (j / steps) * Math.PI;
+    //         generatePoint(u, v, a, b);
+    //     }
+    // }
+
     return vertexList;
 }
+function CreateSurfaceNormalData() {
+    // Значення параметра a
+    const a = 0.5;
+    // Значення параметра b
+    const b = 1;
+    // Кількість кроків циклу побудови
+    const steps = 30;
 
-function r(u, z) {
-    return sqrt(c(z) * c(z) * cos(2 * u) + sqrt(pow(a, 4) - pow(c(z), 4) * pow(sin(2 * u), 2)))
-}
-function c(z) {
-    return 3 * z;
-}
-
-const a = 8
-const scaler = 1;
-
-function cassiniVertex(u, z) {
-    // console.log(r(u, z))
-    let x = r(u, z) * cos(u),
-        y = r(u, z) * sin(u),
-        cZ = z;
-    return [scaler * x, scaler * y, scaler * cZ];
-}
-
-function CreateNormals() {
-    let normalList = [];
-    const NUM_STEPS_U = 50,
-        NUM_STEPS_Z = 50,
-        MAX_U = PI * 2,
-        MAX_Z = 3,
-        STEP_U = MAX_U / NUM_STEPS_U,
-        STEP_Z = MAX_Z / NUM_STEPS_Z
-    for (let u = 0; u < MAX_U; u += STEP_U) {
-        for (let z = -3; z < MAX_Z; z += STEP_Z) {
-            let vertex = normalAnalytic(u, z)
-            normalList.push(...vertex)
-            vertex = normalAnalytic(u + STEP_U, z)
-            normalList.push(...vertex)
-            vertex = normalAnalytic(u, z + STEP_Z)
-            normalList.push(...vertex)
-            normalList.push(...vertex)
-            vertex = normalAnalytic(u + STEP_U, z)
-            normalList.push(...vertex)
-            vertex = normalAnalytic(u + STEP_U, z + STEP_Z)
-            normalList.push(...vertex)
+    // Побудова "горизонтальних кіл" фігури
+    for (let i = 0; i <= steps; i++) {
+        const u = (i / steps) * Math.PI;
+        const uStep = (1 / steps) * Math.PI;
+        for (let j = 0; j <= steps; j++) {
+            const v = (j / steps) * Math.PI * 2;
+            const vStep = (1 / steps) * Math.PI * 2;
+            generatePointsNormal(u, v, a, b);
+            generatePointsNormal(u + uStep, v, a, b);
+            generatePointsNormal(u, v + vStep, a, b);
+            generatePointsNormal(u, v + vStep, a, b);
+            generatePointsNormal(u + uStep, v, a, b);
+            generatePointsNormal(u + uStep, v + vStep, a, b);
         }
     }
+
+
+    // Побудова "вертикальних кіл" фігури
+    // for (let i = 0; i <= steps; i++) {
+    //     const v = (i / steps) * Math.PI * 2;
+    //     for (let j = 0; j <= steps; j++) {
+    //         const u = (j / steps) * Math.PI;
+    //         generatePoint(u, v, a, b);
+    //     }
+    // }
+
     return normalList;
 }
-const e = 0.0001
-function normalAnalytic(u, z) {
-    let u1 = cassiniVertex(u, z),
-        u2 = cassiniVertex(u + e, z),
-        z1 = cassiniVertex(u, z),
-        z2 = cassiniVertex(u, z + e);
-    const dU = [], dZ = []
-    for (let i = 0; i < 3; i++) {
-        dU.push((u1[i] - u2[i]) / e)
-        dZ.push((z1[i] - z2[i]) / e)
-    }
-    const n = m4.normalize(m4.cross(dU, dZ))
-    return n
-}
+
+
 
 
 /* Initialize the WebGL context. Called from init() */
@@ -195,13 +229,15 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
-    shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
+    shProgram.iAttribNormalVertex = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iNormalMat = gl.getUniformLocation(prog, "normalMat");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
     shProgram.iColor = gl.getUniformLocation(prog, "color");
 
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData(), CreateNormals());
+    surface.BufferData(CreateSurfaceData());
+    surface.BufferNormalData(CreateSurfaceNormalData());
+    console.log(CreateSurfaceNormalData())
 
     gl.enable(gl.DEPTH_TEST);
 }
